dda0f85bd736bdd229ef6f59de8b9092dd4f8849
MSunaam <95274859+MSunaam@users.noreply.github.com>
2022-03-07 18:20:09 +0500

    Neat
    Made changes to make it neater

---
+++ colors.py
@@ -0,0 +1,15 @@
+#for colors---generating a dict with keys for tiles and their specific RGB values
+color2 = (237, 228, 218),
+color4 = (237, 224, 200),
+color8 = (242, 178, 120),
+color16 = (246, 150, 98),
+color32 = (246, 125, 95),
+color64 = (246, 94, 58),
+color128 = (248, 246, 241),
+color256 = (238, 204, 97),
+color512 = (237, 200, 80),
+color1024 = (237, 197, 63),
+color2048 = (237, 194, 46),
+text = (118, 110, 101)
+
+colorDic = {2:color2,4:color4,8:color8,16:color16,32:color32,64:color64,128:color128,256:color256,512:color512,1024:color1024,2048:color2048,'text':text}
---
+++ values.py
@@ -0,0 +1,19 @@
+#for main screen height and width
+window_width = 1200
+window_height = 800
+
+#for center black grids height and width
+grid_width = 500
+grid_height = 500
+#margin from top and left
+grid_top = 200
+grid_left = 350
+
+
+#x y coordinates of each tile
+coordinates = [
+[(370,220),(490,220),(610,220),(730,220)],
+[(370,340),(490,340),(610,340),(730,340)],
+[(370,460),(490,460),(610,460),(730,460)],
+[(370,580),(490,580),(610,580),(730,580)],
+]
